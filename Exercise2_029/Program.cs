69de03a5f386fee97468dc349b020d1a0ced81a5
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2022-11-04 16:35:28 +0700

    add method input

--- Exercise2_029/Program.cs
+++ Exercise2_029/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,32 @@ namespace Exercise2_029
 {
     internal class Program
     {
+        private int[] alif = new int[31];
+
+        private int n;
+
+        private int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 31))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 31 elements.\n");
+            }
+           
+        }
+
+ 
+
         static void Main(string[] args)
         {
+
         }
     }
 }
